481953d739a987bd64f169b6807ece1f2bf8c7a1
carpediem511 <vampire05@yandex.ru>
2024-02-07 21:46:20 +0300

    //

--- pages/burgers/index.jsx
+++ pages/burgers/index.jsx
@@ -1,4 +1,15 @@
-const Burgers = () => {
+export const getStaticProps = async () => {
+
+	const result = await fetch('http://localhost:5000/items')
+	const data = await result.json()
+
+	return {
+		props: { burgers: data }
+	}
+}
+
+const Burgers = (props) => {
+	console.log('props-', props)
 	return (
 		<>
 			<h1>Наши бургеры</h1>
---
+++ pages/reviews.jsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+const Reviews = ({ reviews }) => {
+
+	return (
+		<>
+			<Head>
+				<title>Жирные бургеры | Главная</title>
+				<meta name="title" content="Жирные бургеры" />
+			</Head>
+
+			<div>
+				<h1>Отзывы клиентов</h1>
+				<div className="reviews">
+					{!!reviews.length && reviews.slice(0, 20).map(comment => {
+						return (
+							<div key={comment.id} className="review">
+								{comment.id} {' '}
+								{`${comment.body.slice(0, 90)}...`}
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</>
+
+	)
+}
+
+export async function getServerSideProps() {
+
+	const response = await fetch('https://jsonplaceholder.typicode.com/comments')
+	const data = await response.json()
+	return {
+		props: {
+			reviews: data.slice(0, 20)
+		}
+	}
+}
+
+export default Reviews
